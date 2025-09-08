ad89eb62447c738b2ba490688895156841e1d44f
darcynoa <noahdarcy.dev@gmail.com>
2025-09-08 10:31:37 -0400

    new hero animation!

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,15 +1,107 @@
+import Test from "../Test";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+import SplitText from "gsap/SplitText";
+import { useRef } from "react";
+
+gsap.registerPlugin(DrawSVGPlugin, SplitText);
+
 export default function Hero() {
+  const wrapParty = useRef(null);
+  const finalText = useRef(null);
+  const line = useRef(null);
+  const merryTime = useRef(null);
+
+  useGSAP(() => {
+    gsap.set(merryTime.current, { autoAlpha: 0, y: 50 });
+
+    const splitFinalText = new SplitText(finalText.current, {
+      type: "lines",
+    });
+
+    gsap.set(splitFinalText.lines, { autoAlpha: 0, y: 20 });
+
+    const tl = gsap
+      .timeline()
+      .fromTo(
+        wrapParty.current,
+        {
+          autoAlpha: 0,
+          y: 50,
+        },
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 1.5,
+          ease: "bounce.out",
+        },
+      )
+      .fromTo(
+        ".stroke-shape",
+        {
+          drawSVG: "0%",
+          strokeWidth: 4,
+        },
+        {
+          drawSVG: "100%",
+          duration: 1.3,
+          ease: "expo.inOut",
+        },
+      )
+      .to(
+        [wrapParty.current, line.current],
+        {
+          display: "none",
+          autoAlpha: 0,
+        },
+        "+=0.5",
+      )
+      .to(
+        splitFinalText.lines,
+        {
+          autoAlpha: 1,
+          y: 0,
+          duration: 1.5,
+          stagger: 0.25,
+          ease: "expo.inOut",
+        },
+        "-=0.4",
+      )
+      .to(merryTime.current, {
+        autoAlpha: 1,
+        y: 0,
+        duration: 1.5,
+        ease: "expo.inOut",
+      });
+    tl.play();
+  });
   return (
-    <section className="relative w-full h-screen leading-[1] flex flex-col justify-center items-center bg-[linear-gradient(to_bottom,rgba(7,6,6,0)_61%,rgba(7,6,6,1)_100%),url(/home-hero.png)] bg-cover bg-top -mb-[2rem] -mt-[1%] -z-20">
-      <h1 className="flex justify-center drop-shadow-[0px_0px_34px_rgba(7,6,6,1)] items-center font-display text-white font-black text-[5rem] lg:text-[9.5rem] uppercase text-center pb-[4rem] px-[2rem] lg:px-0 leading-[1]">
+    <section className="relative -z-20 -mt-[1%] -mb-[2rem] flex h-screen w-full flex-col items-center justify-center bg-[linear-gradient(to_bottom,rgba(7,6,6,0)_61%,rgba(7,6,6,1)_100%),url(/home-hero.png)] bg-cover bg-top leading-[1]">
+      <h1 className="font-display flex items-center justify-center px-[2rem] pb-[4rem] text-center text-[5rem] leading-[1] font-black text-white uppercase drop-shadow-[0px_0px_34px_rgba(7,6,6,1)] lg:px-0 lg:text-[9.5rem]">
         When We&apos;re <br /> At The Helm
       </h1>
-      <p className="font-sans font-normal uppercase w-[30ch] text-[15px] lg:text-[1.8rem] text-center drop-shadow-[0px_0px_24px_rgba(7,6,6,1)] leading-[1.2] text-white pb-[1rem]">
-        You know it&apos;s going to be a safe &amp; collaborative space to make
-        bold meaningful films
-      </p>
-      <p className="font-handwriting text-[15px] lg:text-[1.8rem] drop-shadow-[0px_0px_24px_rgba(7,6,6,1)] text-white pl-[40%] rotate-12">
-        ~~and have a merry time doing it!
+      <div className="relative flex flex-col">
+        <p
+          ref={finalText}
+          className="w-[30ch] text-center font-sans text-[15px] leading-[1.2] font-normal text-white uppercase drop-shadow-[0px_0px_24px_rgba(7,6,6,1)] lg:text-[1.8rem]"
+        >
+          You know it&apos;s going to be <br /> a safe &amp; collaborative space
+          to make bold films
+        </p>
+        <p
+          ref={wrapParty}
+          className="absolute w-[30ch] text-center font-sans text-[15px] leading-[1.2] font-normal text-white uppercase drop-shadow-[0px_0px_24px_rgba(7,6,6,1)] lg:text-[1.8rem]"
+        >
+          You know there&apos;s gonna be a great wrap party
+        </p>
+        <Test ref={line} className="absolute -top-[2rem] left-0 w-[100%]" />
+      </div>
+      <p
+        ref={merryTime}
+        className="font-handwriting rotate-12 pl-[40%] text-[15px] text-white drop-shadow-[0px_0px_24px_rgba(7,6,6,1)] lg:text-[1.8rem]"
+      >
+        and have a merry time doing it!
       </p>
     </section>
   );
--- components/home/JustSpaceyDescription.jsx
+++ components/home/JustSpaceyDescription.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function JustSpaceyDescription() {
   return (
-    <section className="w-full text-white flex flex-col">
+    <section className="flex w-full flex-col text-white">
       {/* First row: 3 columns */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center">
-        <p className="font-sans font-normal uppercase text-[15px] lg:text-[1.8rem] text-white self-start pt-[4rem]">
+      <div className="grid grid-cols-1 place-items-center gap-4 md:grid-cols-3">
+        <p className="self-start pt-[4rem] font-sans text-[15px] font-normal text-white uppercase lg:text-[1.8rem]">
           logline
         </p>
         <Image
@@ -14,7 +14,7 @@ export default function JustSpaceyDescription() {
           width={406}
           height={540}
         />
-        <p className="font-sans font-normal uppercase text-[15px] lg:text-[1.8rem] text-white self-start pt-[4rem]">
+        <p className="self-start pt-[4rem] font-sans text-[15px] font-normal text-white uppercase lg:text-[1.8rem]">
           When an orderly mortician is forced to bring his recently suspended
           10-year-old daughter with raging ADHD to the morgue, they must work
           together to ensure that the resulting chaos does not see the light of
@@ -22,29 +22,29 @@ export default function JustSpaceyDescription() {
         </p>
       </div>
       <Image
-        src="/cookie-is-bored.png"
+        src="/cookie-is-bored.jpg"
         alt="I mean, you can't see it but Cookie is bored af"
         width={1920}
         height={924}
         className="w-full"
       />
       {/* Second row: 2 columns */}
-      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
+      <div className="grid grid-cols-1 place-items-center md:grid-cols-2">
         <Image
-          src="/body-drawers.png"
+          src="/body-drawers.jpg"
           alt="Nice looking body drawers, for no reason at all"
           width={960}
           height={863}
         />
-        <p className="w-[50%] font-sans font-normal uppercase text-[15px] lg:text-[1.8rem] text-white pt-[4rem] mix-blend-exclusion">
+        <p className="w-[50%] pt-[4rem] font-sans text-[15px] font-normal text-white uppercase mix-blend-exclusion lg:text-[1.8rem]">
           ADHD characters are rarely seen on screen – let alone is there any
           mention of the symptoms that can be an advantage rather than a
           constant pain.
         </p>
       </div>
       {/* Third row: 2 columns */}
-      <div className="grid grid-cols-1 md:grid-cols-2 place-items-center">
-        <p className="w-[50%] font-sans font-normal uppercase text-[15px] lg:text-[1.8rem] text-white pt-[4rem] mix-blend-exclusion">
+      <div className="grid grid-cols-1 place-items-center md:grid-cols-2">
+        <p className="w-[50%] pt-[4rem] font-sans text-[15px] font-normal text-white uppercase mix-blend-exclusion lg:text-[1.8rem]">
           Just Spacey is a morgue-comedy short film that touches on the
           unexpected humor, chaos, and brilliance of thinking differently.
         </p>
