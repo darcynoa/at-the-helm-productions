b71300059b1328f966894043a05aff960579f704
darcynoa <noahdarcy.dev@gmail.com>
2025-11-24 16:17:19 -0500

    shouldn't be able to click the picture on mobile

--- components/home/JustSpaceyTitle.jsx
+++ components/home/JustSpaceyTitle.jsx
@@ -112,12 +112,15 @@ export default function JustSpaceyTitle() {
         className="marker-2 relative h-screen w-full overflow-hidden"
       >
         {/* background image fills the container */}
-        <CustomLink href={"/justspacey"} className="block h-full w-full">
+        <CustomLink
+          href={"/justspacey"}
+          className="pointer-events-none block h-full w-full lg:pointer-events-auto"
+        >
           <Image
             src="/justspacey-hero.png"
             alt="Just Spacey Hero Background"
             fill
-            className="pointer-events-none hidden touch-none object-cover lg:pointer-events-auto lg:block"
+            className="hidden object-cover lg:pointer-events-auto lg:block"
           />
           <div className="h-screen w-full bg-[linear-gradient(to_bottom,rgba(7,6,6,1)_-1%,rgba(7,6,6,0.5)_10%,rgba(7,6,6,0.1)_20%,rgba(7,6,6,0)_22%,rgba(7,6,6,0)_70%,rgba(7,6,6,0.25)_75%,rgba(7,6,6,0.65)_82%,rgba(7,6,6,1)_92%),url(/just-spacey-title-mobile.JPG)] bg-cover bg-[15%]"></div>
         </CustomLink>
