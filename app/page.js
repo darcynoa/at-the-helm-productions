9f0cb3bdcb1e93f6811138cd630c78d959676802
darcynoa <noahdarcy.dev@gmail.com>
2025-11-24 18:40:34 -0500

    force activation upon pointermove event

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
+import { useEffect } from "react";
 import Hero from "@/components/home/Hero";
-
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -13,6 +13,15 @@ import WhosRunningTheShip from "@/components/home/WhosRunningTheShip";
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 export default function Home() {
+  useEffect(() => {
+    function activate() {
+      console.log("Page activated, power level at 9000");
+    }
+
+    window.addEventListener("pointermove", activate, { once: true });
+    return () => window.removeEventListener("pointermove", activate);
+  }, []);
+
   return (
     <>
       <Hero />
