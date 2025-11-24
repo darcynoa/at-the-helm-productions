e5ded6c602fff6e659df91f409fd1cfd8d36b9a9
darcynoa <noahdarcy.dev@gmail.com>
2025-11-24 02:49:47 -0500

    changing blurb wordage

--- components/justspacey/AuthenticLocations.jsx
+++ components/justspacey/AuthenticLocations.jsx
@@ -73,16 +73,15 @@ export default function AuthenticLocations() {
             ref={locationName}
             className="font-display text-center text-[3rem] font-black text-white uppercase lg:text-[7rem]"
           >
-            Pet Passages
+            Real Crematory
           </h2>
           <div className="flex flex-col items-start justify-between gap-[1rem] px-[0.5rem] lg:flex-row lg:items-center lg:gap-0 lg:px-[5rem]">
             <h4 className="w-1/2 font-sans text-[0.95rem] leading-[1.2] text-white uppercase lg:w-1/3 lg:text-[2.5rem]">
-              Thanks to the kind and generous folks at Pet Passages – Livonia,
-              we were able to film at a{" "}
+              Thanks for the kind and generous folks at{" "}
               <span className="font-handwriting text-cyan text-[1rem] lg:text-[length:inherit]">
-                real crematory
+                Pet Passages – Livonia
               </span>
-              .
+              , for bringing this dream to life.
             </h4>
             <Link
               className="font-sans text-[15px] font-normal text-white uppercase mix-blend-exclusion transition-shadow duration-300 ease-in-out hover:drop-shadow-[4px_4px_14px_rgba(5,255,192,1)] lg:text-[1.8rem]"
