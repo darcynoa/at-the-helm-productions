5394f2aa59bb3a7ae90dfb8cc9bdc74f11627297
darcynoa <noahdarcy.dev@gmail.com>
2026-03-19 02:31:08 -0400

    took the footer out of the Sanity Studio

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,19 +6,24 @@ import { usePathname } from "next/navigation";
 export default function Footer() {
   const pathname = usePathname();
   const isJustSpacey = pathname === "/justspacey";
+  const isStudio =
+    typeof window !== "undefined" &&
+    window.location.pathname.startsWith("/studio");
 
   return (
-    <footer
-      id="footer"
-      className="flex items-center justify-between px-[1rem] py-[1rem] font-sans text-[0.5rem] text-white uppercase lg:px-[5rem] lg:text-[1rem]"
-    >
-      <Link
-        href="/"
-        className={`${isJustSpacey ? "opacity-100" : "opacity-0"} font-handwriting text-cyan text-[1.5rem] transition-shadow duration-300 ease-in-out hover:drop-shadow-[4px_4px_14px_rgba(5,255,192,1)] lg:text-[3.5rem]`}
+    !isStudio && (
+      <footer
+        id="footer"
+        className="flex items-center justify-between px-[1rem] py-[1rem] font-sans text-[0.5rem] text-white uppercase lg:px-[5rem] lg:text-[1rem]"
       >
-        Back to Home
-      </Link>
-      © copyright 2025 All Rights Reserved.
-    </footer>
+        <Link
+          href="/"
+          className={`${isJustSpacey ? "opacity-100" : "opacity-0"} font-handwriting text-cyan text-[1.5rem] transition-shadow duration-300 ease-in-out hover:drop-shadow-[4px_4px_14px_rgba(5,255,192,1)] lg:text-[3.5rem]`}
+        >
+          Back to Home
+        </Link>
+        © copyright 2025 All Rights Reserved.
+      </footer>
+    )
   );
 }
